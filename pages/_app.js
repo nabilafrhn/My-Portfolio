0d32f894f8e167a642298c23c1d8061e6bec4d53
“nabilafrhn” <nabilafarahana01@gmail.com>
2023-02-23 12:09:48 +0700

    update pt-2

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,11 @@
-import Navbar from '../components/Navbar';
+
 import '../styles/globals.css';
 import '../components/footer';
-import Footer from '../components/footer';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Navbar />
       <Component {...pageProps} />
-      <Footer />
     </>
   );
 }
--- pages/work.jsx
+++ pages/work.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import Footer from '../components/footer'
 import Hero from '../components/Hero'
+import Navbar from '../components/Navbar'
 import Portfolio from '../components/Portfolio'
 
 const work = () => {
   return (
     <div>
+      <Navbar />
       <Hero heading='About me' message='This is some information about me' />
         <Portfolio />
+      <Footer />
     </div>
   )
 }
