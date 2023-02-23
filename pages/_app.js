e3278d4808dfebd72a8f23d2c70343b0ecfbeff3
“nabilafrhn” <nabilafarahana01@gmail.com>
2023-02-23 12:16:37 +0700

    update pt-3

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 
 import '../styles/globals.css';
 import '../components/footer';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Component {...pageProps} />
+    <Navbar />
+      <Component {...pageProps} />\
+      <Footer />
     </>
   );
 }
--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import Contact from '../components/Contact'
-import Footer from '../components/footer'
 import Hero from '../components/Hero'
-import Navbar from '../components/Navbar'
 
 const contact = () => {
   return (
     <div>
-        <Navbar/>
         <Hero heading='Contact' message='Submit the form below for more work and quotes.' />
         <Contact />
-        <Footer />
     </div>
   )
 }
